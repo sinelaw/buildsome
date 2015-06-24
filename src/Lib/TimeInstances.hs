de51702c42d4368a4aa9862c2dabd331169edb59
Noam Lewis <noam.lewis@elastifile.com>
2015-06-24 12:47:47 +0300

    use host-order serialization using word 64 for TimeDiff

--- src/Lib/TimeInstances.hs
+++ src/Lib/TimeInstances.hs
@@ -4,24 +4,23 @@ module Lib.TimeInstances () where
 
 import Control.Applicative ((<$>))
 import Data.Binary(Binary(..))
+import Data.Binary.Get (getWord64host)
+import Data.Binary.Put (putWord64host)
 import Data.Time.Clock (NominalDiffTime, DiffTime)
 import Data.Fixed (Pico, Fixed(..), E12)
+import Data.Word (Word64)
 
-toPicos :: Pico -> Integer
-fromPicos :: Integer -> Pico
-#if __GLASGOW_HASKELL__ <= 706
+toPicos :: Pico -> Word64
+fromPicos :: Word64 -> Pico
 toPicos = truncate . (*1e12)
 fromPicos = (/1e12) . realToFrac
-#else
-toPicos (MkFixed x) = x
-fromPicos = MkFixed
-#endif
+
 {-# INLINE toPicos #-}
 {-# INLINE fromPicos #-}
 
 instance Binary (Fixed E12) where
-  get = fromPicos <$> get
-  put = put . toPicos
+  get = fromPicos <$> getWord64host
+  put = putWord64host . toPicos
   {-# INLINE get #-}
   {-# INLINE put #-}
 
