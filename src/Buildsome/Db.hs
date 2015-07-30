afdbaad805cdc3ab1807a6a1ea6b8705bdb13fb2
Noam Lewis <noam.lewis@elastifile.com>
2015-07-30 16:25:23 +0300

    use ExecutionSummary + InputSummary instead of ExecutionLog

--- src/Buildsome/Db.hs
+++ src/Buildsome/Db.hs
@@ -1,60 +1,66 @@
+{-# LANGUAGE DeriveFoldable    #-}
+{-# LANGUAGE DeriveFunctor     #-}
+{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE DeriveTraversable #-}
 {-# LANGUAGE NoImplicitPrelude #-}
-{-# LANGUAGE DeriveGeneric, OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards   #-}
 module Buildsome.Db
   ( Db, with
   , registeredOutputsRef, leakedOutputsRef
-  , InputDesc(..), FileDesc(..)
+  , InputDesc(..), FileDesc(..), InputSummary(..)
   , OutputDesc(..)
   , ExecutionLog(..), executionLog
+  , ExecutionSummary(..), executionSummary
+  , summarizeExecutionLog
   , FileContentDescCache(..), fileContentDescCache
   , Reason(..)
   , IRef(..)
   , MFileContentDesc, MakefileParseCache(..), makefileParseCache
   ) where
 
-import Prelude.Compat hiding (FilePath)
+import           Prelude.Compat hiding (FilePath)
 
-import Buildsome.BuildId (BuildId)
-
-import Data.Binary (Binary(..))
-import Data.ByteString (ByteString)
-import Data.Default (def)
-import Data.IORef
-import Data.Map (Map)
-import Data.Monoid ((<>))
-import Data.Set (Set)
-import Data.Time.Clock (DiffTime)
-import Data.Time.Clock.POSIX (POSIXTime)
-import GHC.Generics (Generic)
-import Lib.Binary (encode, decode)
-import Lib.Directory (catchDoesNotExist, createDirectories, makeAbsolutePath)
-import Lib.Exception (bracket)
-import Lib.FileDesc (FileContentDesc, FileModeDesc, FileStatDesc)
-import Lib.FilePath (FilePath, (</>), (<.>))
-import qualified Lib.FSHook as FSHook
-import Lib.Makefile (Makefile)
-import Lib.Makefile.Monad (PutStrLn)
-import Lib.StdOutputs (StdOutputs(..))
-import Lib.TimeInstances ()
+import           Buildsome.BuildId (BuildId)
+import           Data.Binary (Binary(..))
+import           Data.ByteString (ByteString)
+import           Data.Default (def)
+import           Data.IORef
+import           Data.Map (Map)
+import           Data.Monoid ((<>))
+import           Data.Set (Set)
+import           Data.Time.Clock (DiffTime)
+import           Data.Time.Clock.POSIX (POSIXTime)
+import           GHC.Generics (Generic)
+import           Lib.Binary (encode, decode)
+import           Lib.Directory (catchDoesNotExist, createDirectories, makeAbsolutePath)
+import           Lib.Exception (bracket)
+import           Lib.FileDesc (FileContentDesc, FileModeDesc, FileStatDesc(..), fileModeDescOfStatDesc)
+import           Lib.FilePath (FilePath, (<.>), (</>))
+import           Lib.Makefile (Makefile)
+import           Lib.Makefile.Monad (PutStrLn)
+import           Lib.StdOutputs (StdOutputs(..))
+import           Lib.TimeInstances ()
 import qualified Crypto.Hash.MD5 as MD5
 import qualified Data.ByteString.Char8 as BS8
 import qualified Data.Set as S
 import qualified Database.LevelDB.Base as LevelDB
+import qualified Lib.FSHook as FSHook
 import qualified Lib.Makefile as Makefile
 import qualified System.Posix.ByteString as Posix
 
 schemaVersion :: ByteString
-schemaVersion = "schema.ver.19.no_ad.mtime_outdesc"
+schemaVersion = "schema.ver.20.input_summary"
 
 data Db = Db
-  { dbLevel :: LevelDB.DB
+  { dbLevel             :: LevelDB.DB
   , dbRegisteredOutputs :: IORef (Set FilePath)
-  , dbLeakedOutputs :: IORef (Set FilePath)
+  , dbLeakedOutputs     :: IORef (Set FilePath)
   }
 
 data FileContentDescCache = FileContentDescCache
   { fcdcModificationTime :: POSIXTime
-  , fcdcFileContentDesc :: FileContentDesc
+  , fcdcFileContentDesc  :: FileContentDesc
   } deriving (Generic, Show)
 instance Binary FileContentDescCache
 
@@ -71,33 +77,73 @@ instance Binary Reason
 
 
 data InputDesc = InputDesc
-  { idModeAccess :: Maybe (Reason, FileModeDesc)
-  , idStatAccess :: Maybe (Reason, FileStatDesc)
+  { idModeAccess    :: Maybe (Reason, FileModeDesc)
+  , idStatAccess    :: Maybe (Reason, FileStatDesc)
   , idContentAccess :: Maybe (Reason, FileContentDesc)
   } deriving (Generic, Show)
 instance Binary InputDesc
 
 data FileDesc ne e
-  = FileDescNonExisting ne
-  | FileDescExisting e
-  deriving (Generic, Eq, Ord, Show)
+  = FileDescNonExisting !ne
+  | FileDescExisting !e
+  deriving (Generic, Eq, Ord, Show, Functor, Foldable, Traversable)
 instance (Binary ne, Binary e) => Binary (FileDesc ne e)
 
+bimapFileDesc :: (ne -> ne') -> (e -> e') -> FileDesc ne e -> FileDesc ne' e'
+bimapFileDesc f _ (FileDescNonExisting x) = FileDescNonExisting (f x)
+bimapFileDesc _ g (FileDescExisting x) = FileDescExisting (g x)
+
 data OutputDesc = OutputDesc
-  { odStatDesc :: FileStatDesc
+  { odStatDesc    :: FileStatDesc
   , odContentDesc :: Maybe FileContentDesc -- Nothing if directory
   } deriving (Generic, Show)
 instance Binary OutputDesc
 
+data InputSummary
+  = InputMTime POSIXTime
+  -- For dirs, this is for stat/existence only check
+  | InputModeDesc FileModeDesc
+  deriving (Generic, Show)
+instance Binary InputSummary
+
+data ExecutionSummary = ExecutionSummary
+  { esInputsDescs  :: Map FilePath (FileDesc () InputSummary)
+  , esOutputsDescs :: Map FilePath (FileDesc () (POSIXTime, OutputDesc))
+  , esSelfTime     :: DiffTime
+  , esStdErr       :: ByteString
+  } deriving (Generic, Show)
+instance Binary ExecutionSummary
+
 data ExecutionLog = ExecutionLog
-  { elBuildId :: BuildId
-  , elCommand :: ByteString -- Mainly for debugging
-  , elInputsDescs :: Map FilePath (FileDesc Reason (POSIXTime, InputDesc))
+  { elBuildId      :: BuildId
+  , elCommand      :: ByteString -- Mainly for debugging
+  , elInputsDescs  :: Map FilePath (FileDesc Reason (POSIXTime, InputDesc))
   , elOutputsDescs :: Map FilePath (FileDesc () (POSIXTime, OutputDesc))
-  , elStdoutputs :: StdOutputs ByteString
-  , elSelfTime :: DiffTime
+  , elStdoutputs   :: StdOutputs ByteString
+  , elSelfTime     :: DiffTime
   } deriving (Generic, Show)
 instance Binary ExecutionLog
+
+summarizeExecutionLog :: ExecutionLog -> ExecutionSummary
+summarizeExecutionLog ExecutionLog{..} =
+  ExecutionSummary
+  { esInputsDescs = bimapFileDesc (const ()) summarizeInput <$> elInputsDescs
+  , esOutputsDescs = elOutputsDescs
+  , esSelfTime = elSelfTime
+  , esStdErr = stdErr elStdoutputs
+  }
+  where
+    summarizeInput (mtime, inputDesc) =
+      case inputDesc of
+        InputDesc
+          { idModeAccess = Just (_, modeDesc)
+          , idStatAccess = Nothing
+          , idContentAccess = Nothing } -> InputModeDesc modeDesc
+        InputDesc { idContentAccess = Just _ } -> InputMTime mtime
+        InputDesc { idStatAccess = Just (_, s@FileStatDirectory{}) } ->
+            InputModeDesc $ fileModeDescOfStatDesc s
+        InputDesc { idStatAccess = Just _ } -> InputMTime mtime
+        InputDesc Nothing Nothing Nothing -> error "Input accessed but no access type?!"
 
 registeredOutputsRef :: Db -> IORef (Set FilePath)
 registeredOutputsRef = dbRegisteredOutputs
@@ -145,9 +191,9 @@ with rawDbPath body = do
       `catchDoesNotExist` return S.empty
 
 data IRef a = IRef
-  { readIRef :: IO (Maybe a)
+  { readIRef  :: IO (Maybe a)
   , writeIRef :: a -> IO ()
-  , delIRef :: IO ()
+  , delIRef   :: IO ()
   }
 
 mkIRefKey :: Binary a => ByteString -> Db -> IRef a
@@ -157,10 +203,19 @@ mkIRefKey key db = IRef
   , delIRef = deleteKey db key
   }
 
+data ExecutionLogType = ExecutionLogTypeSummary  | ExecutionLogTypeFull
+    deriving (Generic)
+
+instance Binary ExecutionLogType
+
+targetKey :: Makefile.TargetType output input -> ByteString
+targetKey = MD5.hash . Makefile.targetCmds
+
 executionLog :: Makefile.Target -> Db -> IRef ExecutionLog
-executionLog target = mkIRefKey targetKey
-  where
-    targetKey = MD5.hash $ Makefile.targetCmds target -- TODO: Canonicalize commands (whitespace/etc)
+executionLog = mkIRefKey . (encode ExecutionLogTypeFull <>) . targetKey
+
+executionSummary :: Makefile.Target -> Db -> IRef ExecutionSummary
+executionSummary = mkIRefKey . (encode ExecutionLogTypeSummary <>) .  targetKey
 
 fileContentDescCache :: FilePath -> Db -> IRef FileContentDescCache
 fileContentDescCache = mkIRefKey
