fa948b94f2b5737476053bf70685dc7b0b4c6032
Noam Lewis <noam.lewis@elastifile.com>
2015-08-09 20:33:47 +0300

    schema.ver.21.i

--- src/Buildsome/Db.hs
+++ src/Buildsome/Db.hs
@@ -44,7 +44,7 @@ import qualified Lib.Makefile as Makefile
 import qualified System.Posix.ByteString as Posix
 
 schemaVersion :: ByteString
-schemaVersion = "schema.ver.19"
+schemaVersion = "schema.ver.21.i"
 
 data Db = Db
   { dbLevel :: LevelDB.DB
